9e6c8f1a68ac10474c58bb8bc7821e0dba60bcdc
Shane Weickum <sweick23@outlook.com>
2017-12-09 17:26:35 -0800

    10-print

---
+++ sketch.js
@@ -0,0 +1,24 @@
+let x = 0;
+let y = 0;
+let spacing = 15;
+
+function setup(){
+createCanvas(600,600);
+background(0);
+}
+
+function draw(){
+	stroke(255);
+if(random(1) < 0.5){
+	line(x, y, x + spacing, y + spacing);
+}else{
+	line(x, y + spacing, x + spacing, y);
+}
+
+x = x + spacing;
+if(x > width){
+	x = 0;
+	y = y + spacing;
+}
+
+}
